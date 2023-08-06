1c5df1569aaa59233fedb15050ba30c804864c3a
HY KIM <chopinoff@gmail.com>
2023-08-06 18:54:39 +0900

    Add: 로그인 시 localStorage에 accessToken 저장 기능 추가

--- src/api/auth/postSignIn.ts
+++ src/api/auth/postSignIn.ts
@@ -7,9 +7,10 @@ async function postSignIn({ email, password }: authPayloadType) {
       email,
       password,
     });
+    window.localStorage.setItem('accessToken', res.data.access_token);
     return res.data;
   } catch (err) {
-    alert('로그인에 실패했습니다. 이메일과 비밀번호를 다시 확인해주세요.');
+    alert('이메일과 비밀번호를 다시 확인해주세요.');
     return null;
   }
 }
