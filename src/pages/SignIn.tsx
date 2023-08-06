8913d4a6453c4e4b77422ed13a89e32b2323e72f
HY KIM <chopinoff@gmail.com>
2023-08-07 00:20:23 +0900

    Add: accessToken이 있는 상태로 로그인, 회원가입 페이지에 접근 시 /todo로 이동하도록 구현

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import EmailPasswordForm from '../components/EmailPasswordForm';
 import postSignIn from '../api/auth/postSignIn';
@@ -5,17 +6,18 @@ import { AuthPayload } from '../types/authTypes';
 
 function SignIn() {
   const navigate = useNavigate();
+  const accessToken = window.localStorage.getItem('accessToken');
+
+  useEffect(() => {
+    accessToken && navigate('/todo');
+  }, [accessToken]);
 
   async function handleSignIn({ email, password }: AuthPayload) {
     const data = await postSignIn({ email, password });
     data !== null && navigate('/todo');
   }
 
-  return (
-    <div>
-      <EmailPasswordForm handleAuth={handleSignIn} />
-    </div>
-  );
+  return <div>{!accessToken && <EmailPasswordForm handleAuth={handleSignIn} />}</div>;
 }
 
 export default SignIn;
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import EmailPasswordForm from '../components/EmailPasswordForm';
 import postSignUp from '../api/auth/postSignUp';
@@ -5,17 +6,18 @@ import { AuthPayload } from '../types/authTypes';
 
 function SignUp() {
   const navigate = useNavigate();
+  const accessToken = window.localStorage.getItem('accessToken');
+
+  useEffect(() => {
+    accessToken && navigate('/todo');
+  }, [accessToken]);
 
   async function handleSignUp({ email, password }: AuthPayload) {
     const data = await postSignUp({ email, password });
     data !== null && navigate('/signin');
   }
 
-  return (
-    <div>
-      <EmailPasswordForm handleAuth={handleSignUp} />
-    </div>
-  );
+  return <div>{!accessToken && <EmailPasswordForm handleAuth={handleSignUp} />}</div>;
 }
 
 export default SignUp;
