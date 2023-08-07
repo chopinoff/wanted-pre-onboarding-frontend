fdccdca352a34518e9680efaf7e0c22a810d310b
HY KIM <chopinoff@gmail.com>
2023-08-07 15:24:36 +0900

    Add: accessToken이 없는 상태로 /todo 페이지에 접근 시 /signin로 이동하도록 구현

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,13 +1,29 @@
+import { useNavigate } from 'react-router-dom';
 import WriteTodoForm from '../components/todos/WriteTodoForm';
 import TodoItemList from '../components/todos/TodoItemList';
+import { useEffect } from 'react';
 
 function Todo() {
+  const accessToken = window.localStorage.getItem('accessToken');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!accessToken) {
+      navigate('/signin');
+      alert('로그인이 필요한 서비스입니다.');
+    }
+  });
+
   return (
-    <div>
-      <h2>Todo</h2>
-      <WriteTodoForm />
-      <TodoItemList />
-    </div>
+    <>
+      {accessToken && (
+        <div>
+          <h2>Todo</h2>
+          <WriteTodoForm />
+          <TodoItemList />
+        </div>
+      )}
+    </>
   );
 }
 
