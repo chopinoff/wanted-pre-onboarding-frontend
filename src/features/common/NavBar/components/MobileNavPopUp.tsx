36d8f5ffe1006ae6d8d879a1fa5f1a4cd228b2f5
HY KIM <chopinoff@gmail.com>
2023-08-16 23:08:12 +0900

    Design: 모바일 팝업 메뉴 배경색 추가

--- src/features/common/NavBar/components/MobileNavPopUp.tsx
+++ src/features/common/NavBar/components/MobileNavPopUp.tsx
@@ -73,6 +73,7 @@ const popUpContainer = (popUpOpen: boolean) => css`
   height: 120px;
   bottom: ${popUpOpen ? '60px' : '-60px'};
   transition: all 0.5s;
+  background-color: var(--bgColor);
 `;
 
 export default MobileNavPopUp;
