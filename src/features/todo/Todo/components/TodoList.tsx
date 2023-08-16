ab8e7afaba1d051ec03747b19fce8325e5fafb03
HY KIM <chopinoff@gmail.com>
2023-08-16 22:25:47 +0900

    Add: TodoList가 존재하지 않을 때 띄워줄 컴포넌트 추가

--- src/features/todo/Todo/components/TodoList.tsx
+++ src/features/todo/Todo/components/TodoList.tsx
@@ -2,6 +2,8 @@ import React, { Dispatch, SetStateAction } from 'react';
 import TodoItem from './TodoItem';
 import { TodosResult } from 'types/todoTypes';
 import TodoSelectDelete from './TodoSelectDelete';
+import { GoSmiley } from 'react-icons/go';
+import { css } from '@emotion/react';
 
 interface Props {
   todoList: TodosResult[];
@@ -17,9 +19,27 @@ function TodoList({ todoList, getTodoList, setTodoList }: Props) {
         {todoList?.map(({ id, todo, isCompleted }) => (
           <TodoItem key={id} {...{ id, todo, isCompleted, getTodoList }} />
         ))}
+        {todoList?.length === 0 && (
+          <div css={noTodoContainer}>
+            <GoSmiley size={24} />
+            <p>작성된 목록이 없습니다</p>
+          </div>
+        )}
       </ul>
     </>
   );
 }
+
+const noTodoContainer = css`
+  width: 100%;
+  height: 260px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  & p {
+    margin-top: 20px;
+  }
+`;
 
 export default TodoList;
